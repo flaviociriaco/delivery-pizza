cb51b6f88cfbd0a316ea19abeeb48277d1b52ce2
Flavio Ciriaco de Moura <flaviociriaco@yahoo.com>
2023-04-25 19:04:37 -0300

    adicionando nome das pizzas na tela

---
+++ script.js
@@ -0,0 +1,10 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+pizzaJson.map((item, index) => {
+  let pizzaItem = c(".models .pizza-item").cloneNode(true);
+
+  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+
+  c(".pizza-area").append(pizzaItem);
+});
