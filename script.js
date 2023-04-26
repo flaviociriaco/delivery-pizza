82608f19897eac0021667991f2ee1ea8570c8a93
Flavio Ciriaco de Moura <flaviociriaco@yahoo.com>
2023-04-26 20:51:46 -0300

    adicionando descrição e preço

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ pizzaJson.map((item, index) => {
   let pizzaItem = c(".models .pizza-item").cloneNode(true);
 
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+  pizzaItem.querySelector(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
 
   c(".pizza-area").append(pizzaItem);
 });
